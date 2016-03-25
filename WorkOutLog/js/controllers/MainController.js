30ad4d1a839c9aed27f1f9d04cd6fe5ad94a79fb
Fernando Fernandez <ffmp777@yahoo.com>
2016-03-24 21:56:01 -0600

    changed counter to 0

--- WorkOutLog/js/controllers/MainController.js
+++ WorkOutLog/js/controllers/MainController.js
@@ -3,37 +3,37 @@ app.controller('MainController', ['$scope', function($scope) {
     {
       icon: 'http://i.imgur.com/89Gr8pq.jpg',
       name: 'Pushups',
-      count: 20
+      count: 0
     },
     {
       icon: 'http://i.imgur.com/e4rVtxm.jpg',
       name: 'Squats',
-      count: 15
+      count: 0
     },
     {
       icon: 'http://i.imgur.com/XAy1YH2.jpg',
       name: 'Pullups',
-      count: 10
+      count: 0
     },
     {
       icon: 'http://i.imgur.com/4aVqAo7.jpg',
       name: 'Rows',
-      count: 8
+      count: 0
     },
     {
       icon: 'http://i.imgur.com/6TJEn0j.jpg',
       name: 'Lunges',
-      count: 10
+      count: 0
     },
     {
       icon: 'http://i.imgur.com/qNsPppE.jpg',
       name: 'Step Ups',
-      count: 10
+      count: 0
     },
     {
       icon: 'http://i.imgur.com/6NsxRpR.jpg',
       name: 'Sit Ups',
-      count: 20
+      count: 0
     }
   ];
 
